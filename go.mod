477fa47e65f999b668b8346e56fba82b1008fdac
Damilare Durojaye <damilare.durojaye@nordsec.com>
2022-01-20 23:12:36 +0100

    initial commit and package oriented encapsulation OOP

---
+++ go.mod
@@ -0,0 +1,3 @@
+module oopGolang
+
+go 1.17
---
+++ gopherpay/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"oopGolang/gopherpay/payment"
+)
+
+func main() {
+
+	credit := payment.CreateCreditAccount(
+		"Debra Ingram",
+		"5555-5555-5555-444",
+		5,
+		2021,
+		345)
+
+	fmt.Printf("Owner name: %v\n", credit.OwnerName())
+	fmt.Printf("Card Number: %v\n", credit.CardNumber())
+
+	err := credit.SetCardNumber("invalid")
+	if err != nil {
+		fmt.Printf("That did't work: %v\n", err)
+	}
+
+	fmt.Printf("Avialable Credit: %v\n", credit.AvailableCredit())
+}
